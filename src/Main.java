851737663d3892136cd3879fb7df3398eb061535
Александр <alexakr1993@gmail.com>
2022-11-17 17:31:14 +0400

    First Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ///Task 1
+        System.out.println("Task 1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника - " + fullName);
+
+        ///Task 2
+        System.out.println("Task 2");
+        String fullname = fullName.toUpperCase();
+        System.out.println("ФИО сотрудника - " + fullname);
+
+        ///Task 3
+        System.out.println("Task 3");
+        String naming = "Иванов Семён Семёнович";
+        naming = naming.replace("ё", "е");
+        System.out.println(naming);
+
+
     }
 }
